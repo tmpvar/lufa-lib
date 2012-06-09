48e9b7e93ebd6eda9a94ea44f3e2fec5c8baf4f9
Elijah Insua <tmpvar@gmail.com>
2012-06-09 14:08:25 -0700

    fix midi interface descriptor

--- trunk/Demos/Device/ClassDriver/tbase8/tbase8.c
+++ trunk/Demos/Device/ClassDriver/tbase8/tbase8.c
@@ -2,6 +2,26 @@
 
 #include "tbase8.h"
 
+/** LUFA MIDI Class driver interface configuration and state information. This structure is
+ *  passed to all MIDI Class driver functions, so that multiple instances of the same class
+ *  within a device can be differentiated from one another.
+ */
+USB_ClassInfo_MIDI_Device_t tbase8_MIDI_Interface = {
+  .Config = {
+    .StreamingInterfaceNumber = 1,
+    .DataINEndpoint           = {
+      .Address          = MIDI_STREAM_IN_EPADDR,
+      .Size             = MIDI_STREAM_EPSIZE,
+      .Banks            = 1,
+    },
+    .DataOUTEndpoint = {
+      .Address          = MIDI_STREAM_OUT_EPADDR,
+      .Size             = MIDI_STREAM_EPSIZE,
+      .Banks            = 1,
+    },
+  },
+};
+
 
 int main(void) {
   SetupHardware();
